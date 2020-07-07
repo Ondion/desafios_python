6313d79df3d8147d43bba366fdf6f66c42273006
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-06 23:11:59 -0300

    Hello World!
    Primeiro exercício

---
+++ exercicios/exe001.py
@@ -0,0 +1,8 @@
+# Crie um programa que escreva "Olá mundo!" na tela.
+
+print('===== DESAFIO 01 =====\n')
+
+msg = 'Olá mundo!'      # variavel recebe string
+msg_2 = 'Hello World!'
+print(msg)
+print(msg_2)
