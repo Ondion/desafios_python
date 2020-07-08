9e5b67f4a098df419fcfa8aaf17aaeb0dd1a37d4
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-08 01:11:46 -0300

    metros para centimetros e milimetros

---
+++ exercicios/exe008.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milímetros.
+
+
+print('===== DESAFIO 08 =====\n')
+
+metros = int(input('Metros: '))
+print(f'Valor em centimetros {metros * 100}, valor em milimetros {metros * 1000}')      # realizando as conversões
