0b53a229b5a971d831b7f1cd9eabb9cc29517ade
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-07 22:11:58 -0300

    a + b = c

---
+++ exercicios/exe003.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+print('===== DESAFIO 03 =====\n')
+
+a = int(input('Numero: '))      # recebendo as variaveis
+b = int(input('Numero: '))
+
+print(f'{a + b}')       # imprimindo a soma das variaveis
