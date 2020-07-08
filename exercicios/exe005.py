af33277f934f2ff480aa90250fa319620b902844
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-07 22:19:54 -0300

    (-1) <= 1 => (+1)

---
+++ exercicios/exe005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+
+print('===== DESAFIO 05 =====\n')
+
+numero = int(input('Digite o valor: '))
+print(f'Você digitou o numero {numero}, seu antecessor é {numero - 1} e seu sucessor é {numero + 1}')
