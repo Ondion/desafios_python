031643fb3658deab0470551b7ed63ff2325bb00b
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-08 01:17:31 -0300

    conversor de dolares

---
+++ exercicios/exe010.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+print('===== DESAFIO 10 =====\n')
+
+dolar = float(input('Valor em R$: '))
+
+print(f'Valor em reais R$: {dolar:.2f}  valor em dolares $: {(dolar / 5.24):.2f}')
