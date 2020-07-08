9a8dcb123fe3dc4f2106b38d000fcb1e3382b24f
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-08 01:21:32 -0300

    5% de desconto

---
+++ exercicios/exe012.py
@@ -0,0 +1,6 @@
+Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+print('===== DESAFIO 12 =====\n')
+
+desconto = float(input('Valor para desconto R$: '))
+print(f'Você digitou R$:{desconto:.2f} e aplicando 5% de desconto temos: R$:{desconto * 0.95:.2f} ')
