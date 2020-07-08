6f4a5d685432b34c29ea7bc8b6cdf2ad139c2fc7
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-08 01:48:40 -0300

    Celsius para Fahrenheit

---
+++ exercicios/exe014.py
@@ -0,0 +1,6 @@
+# Escreva um programa que converte uma temperatura digitada em Celsius para Fahrenheit.
+
+print('===== DESAFIO 14 =====\n')
+
+tempc = float(input('Digite o valor em graus celsius a ser convertido em fahrenheit: '))
+print(f'O valor de {tempc} graus celsius em fahrenheit é {tempc * 9/5 + 32} graus')
