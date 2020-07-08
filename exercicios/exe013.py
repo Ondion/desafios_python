310cd968652043f83e7c8e974236b4d6ae731226
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-08 01:48:02 -0300

    15 % de aumento

---
+++ exercicios/exe013.py
@@ -0,0 +1,6 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+print('===== DESAFIO 13 =====\n')
+
+salario = float(input('Valor do salario para aumento de 15%: R$: '))
+print(f'Salario com aumento é R$:{salario * 1.15:.2f}')
