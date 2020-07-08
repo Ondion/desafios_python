adbbc81d7fe0e33937969969f482b94ec2d5c9b8
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-07 22:10:30 -0300

    Qual seu nome?

---
+++ exercicios/exe002.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia no nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+print('===== DESAFIO 02 =====\n')
+
+nome = input('Qual seu nome? ')     # capturando string para a variavel nome
+print(f'Prazer em te conhecer {nome}.',)        # imprimindo a variavel
