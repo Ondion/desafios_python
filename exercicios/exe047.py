e9a63bb973edaa918a8d9f1fe85406f204de11d6
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-16 23:49:22 -0300

    numeros pares

---
+++ exercicios/exe047.py
@@ -0,0 +1,7 @@
+# Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+
+
+print('===== DESAFIO 47 =====\n')
+
+for i in range (0, 51, 2):
+    print(i, end=', ')
