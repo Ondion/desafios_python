36513bad3475d43d60004eff2c5bf3b6eee2489e
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-22 19:35:33 -0300

    soma de numeros pares, só os pares

---
+++ exercicios/exe050.py
@@ -0,0 +1,12 @@
+# Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor
+# digitado for impar, desconsidere-o.
+
+print('===== DESAFIO 50 =====\n')
+
+soma = 0
+for i in range(0, 6):
+    numero = int(input(f'\033[1;3{i}mDigite um número inteiro: '))
+    if numero % 2 == 0:
+        soma += numero
+
+print('\n\033[mA soma dos números pares digitados é:', soma)
