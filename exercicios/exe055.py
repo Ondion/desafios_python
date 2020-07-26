b49d56f6601a823cf139ce19c0a36c7b01913cc1
Fabio N. Xavier <fabio.xavier@live.com>
2020-07-25 23:56:07 -0300

    Maior e menor

---
+++ exercicios/exe055.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+
+print('===== DESAFIO 55 =====\n')
+
+maior = menor = peso = float(input('Entre com o peso: '))
+for range in range(0 , 5):
+    peso = float(input('Entre com o peso: '))
+    if peso > maior:
+        maior = peso
+    if peso < menor:
+        menor = peso
+print(f'Maior: {maior} e Menor: {menor}')
