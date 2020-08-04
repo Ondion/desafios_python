49eace6ce79a477e36a5b666ec95886df81889f0
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-03 23:47:07 -0300

    começando

---
+++ exercicios/exe069.py
@@ -0,0 +1,13 @@
+
+
+print('===== DESAFIO 69 =====\n')
+
+while True:
+
+    while True:
+        print('dentro while 2 identado')
+        break
+    print('dentro while 1')
+    break
+
+print('fora identado')
