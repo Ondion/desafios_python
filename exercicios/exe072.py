6d81738cb9cd6ebab24710f26a0ef8fe4c3f0c72
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-06 16:41:16 -0300

    int para string

---
+++ exercicios/exe072.py
@@ -0,0 +1,10 @@
+# Crie um programa que tenha uma tupla totalmente preenchida por extenso, de 0 até 20. Seu programa deverá ler um
+# número pelo teclado (entre 0 e 20) e mostrá-lo por extenso
+
+print('===== DESAFIO 72 =====\n')
+
+tupla = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+'quatorze', 'quinze', 'dezesseis', 'dezessente', 'dezoito', 'dezenove', 'vinte')
+
+
+print(tupla[int(input('n: '))])
