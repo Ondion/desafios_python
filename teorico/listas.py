ff49f726af3fd32ed4552c8e274a6cbd8c4496db
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-08 21:47:14 -0300

    listas

--- teorico/listas.py
+++ teorico/listas.py
@@ -1,15 +1,23 @@
-lista_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10.5]
+lista_numeros_inteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]
+lista_numeros_flutuantes = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10, 10.10]
 lista_animais = ['rato', 'gato', 'cachorro', 'leão', 'elefante']
-lista_mistura = [1, 1.35, 'mistura', True]
+lista_booleanda = [True, False, False, True]
+lista_mistura = [1, 2.2, 'cachorro', True]
+
+print(lista_numeros_inteiros)
+print(lista_numeros_flutuantes)
+print(lista_animais)
+print(lista_booleanda)
+print(lista_mistura)
 
 print(lista_animais[2]) # buscando valor de uma lista pelo indice.
 
-print(sum(lista_numeros)) # sum() realiza a somatória de componentes de uma lista, desde que numéricos
+print(sum(lista_numeros_inteiros)) # sum() realiza a somatória de componentes de uma lista, desde que numéricos
 
 for valor in lista_mistura:
     print(valor) # percorrendo todos os valores de uma lista pelo laço for
 
-print(max(lista_numeros), min(lista_numeros)) # max() e min() retorna o maior e o menos valor numérico
+print(max(lista_numeros_inteiros), min(lista_numeros_inteiros)) # max() e min() retorna o maior e o menos valor numérico
 print(max(lista_animais), min(lista_animais)) # no caso de strings, retorna ordem alfabética
 
 print('gato' in lista_animais) # possivel usar condicional com o IN
