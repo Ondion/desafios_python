89f8972c853049400e62d18f77c33060225d7fbe
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-13 02:49:59 -0300

    Ajustando os arquivos

--- django/flixweb/flixweb/urls.py
+++ django/flixweb/flixweb/urls.py
@@ -1,4 +1,4 @@
-"""igtiflixweb URL Configuration
+"""flixweb URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- django/flixweb/manage.py
+++ django/flixweb/manage.py
@@ -4,9 +4,9 @@ import os
 import sys
 
 
-def main() -> object:
+def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'igtiflixweb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flixweb.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
