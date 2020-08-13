fd7e9669304e04229d70a361ba7b05b4f03c3846
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-12 23:04:06 -0300

    Adicionando os templates

--- Django/igtiflixweb/principal/views.py
+++ Django/igtiflixweb/principal/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from pathlib import Path
 from django.http import HttpResponse
+import os
+
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'principal/index.html')
