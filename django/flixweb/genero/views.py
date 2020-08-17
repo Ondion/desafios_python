4ea4f0e402d9e1b339fa188bf3aa55ef1d05401b
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-17 04:16:59 -0300

    update

--- django/flixweb/genero/views.py
+++ django/flixweb/genero/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
 from . import forms
+from . import models
+from django.http import HttpResponseNotAllowed
 
 # Create your views here.
 
 def cadastro(request):
     form = forms.GeneroForm()
-    if request.method == 'Post':
-        print('Vou salvar os dados')
+    if request.method == 'POST':
         form = forms.GeneroForm(request.POST)
-        print(form)
         if form.is_valid():
             form.save(commit = True)
         else:
             print('ERROR')
-    data_dict = {'form': form}
+    generos_list = models.Genero.objects.order_by('descricao')
+    data_dict = {'form': form, 'generos_records': generos_list}
     return render(request, 'genero/genero.html', data_dict)
+
+
+def delete(request, id):
+    try:
+        models.Genero.objects.filter(id=id).delete()
+        form = forms.GeneroForm()
+        generos_list = models.Genero.objects.order_by('descricao')
+        data_dict = {'form': form, 'generos_records': generos_list}
+        return render(request, 'genero/genero.html', data_dict)
+    except:
+        return HttpResponseNotAllowed()
