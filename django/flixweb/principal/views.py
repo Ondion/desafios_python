f9e595fe4a61c79a2316af3acc17d0454ca36e40
Fabio N. Xavier <fabio.xavier@live.com>
2020-08-21 23:56:07 -0300

    Alterações

--- django/flixweb/principal/views.py
+++ django/flixweb/principal/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from pathlib import Path
 # Create your views here.
 
-
 def index(request):
-    return render(request, 'principal/index.html')
+   return render(request,'principal/index.html')
