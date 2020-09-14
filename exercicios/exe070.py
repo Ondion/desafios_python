403020e284d8a903590098156bc9ebf9223f4989
Fabio N. Xavier <fabio.xavier@live.com>
2020-09-14 09:58:44 -0300

    Update exe070.py

--- exercicios/exe070.py
+++ exercicios/exe070.py
@@ -8,17 +8,21 @@ print('===== DESAFIO 70 =====\n')
 barato = nome_barato = total = mais_de_1k = 0
 
 while True:
-    nome = input('Nome no produto: ')
+    nome = str(input('Nome no produto: '))
     valor = float(input('Preço do produto: '))
 
     if barato == 0:
         barato = valor
+
     if valor < barato:
         barato = valor
         nome_barato = nome
-    if valor > 1000:
+
+    if valor >= 1000:
         mais_de_1k += 1
+
     total += valor
+
     if input('Você deseja continuar? <S> ou <N> ').lower() == 'n':
         print(f'Total R$:{total:.2f}, Custam mais de 1k: {mais_de_1k}, O mais barato: {nome_barato}')
         break
